6e28a93bd173c8941c2ff309a57e77043a4ad663
YehuditEr <y0556774655@gmail.com>
2024-05-21 21:38:55 +0300

    by

--- NodeProject/routers/tasks.js
+++ NodeProject/routers/tasks.js
@@ -4,8 +4,8 @@ const {updateTaskById,deleteTaskById,getAllTasks,addTask,getAllTasksById} = requ
 
 router.get('/',getAllTasks);
 router.post('/',addTask);
+router.get('/:userTaskID',getAllTasksById);
 router.post('/:taskId',updateTaskById);
 router.delete('/:taskId',deleteTaskById);
-router.get('/:userTaskID',getAllTasksById);
 
 module.exports = router;
